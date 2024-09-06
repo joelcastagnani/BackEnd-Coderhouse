a2b13560822bef74ab8b4f603a0e184760572d17
Joel <joelcastagnani@hotmail.com>
2024-09-06 20:46:52 -0300

    70200 - 20/08 - Clase 6 - Servidores web - FIN (hay que ahcer el ejercicio final)

--- clase_6/ejemploParams/index.js
+++ clase_6/ejemploParams/index.js
@@ -29,6 +29,78 @@ const usuarios = [
     edad: 28,
   },
 ];
+const peliculas = [
+  {
+    id: 1,
+    nombre: "el Padrino",
+    genero: "drama",
+  },
+  {
+    id: 2,
+    nombre: "inception",
+    genero: "drama",
+  },
+  {
+    id: 3,
+    nombre: "parásitos",
+    genero: "suspenso",
+  },
+  {
+    id: 4,
+    nombre: "toy Story",
+    genero: "drama",
+  },
+  {
+    id: 5,
+    nombre: "la la land",
+    genero: "suspenso",
+  },
+  {
+    id: 6,
+    nombre: "gladiador",
+    genero: "accion",
+  },
+  {
+    id: 7,
+    nombre: "el señor de los anillos",
+    genero: "accion",
+  },
+  {
+    id: 8,
+    nombre: "matrix",
+    genero: "accion",
+  },
+  {
+    id: 9,
+    nombre: "el caballero oscuro",
+    genero: "accion",
+  },
+  {
+    id: 10,
+    nombre: "titanic",
+    genero: "romance",
+  },
+  {
+    id: 11,
+    nombre: "el gran hotel budapest",
+    genero: "romance",
+  },
+  {
+    id: 12,
+    nombre: "el silencio de los inocentes",
+    genero: "romance",
+  },
+  {
+    id: 13,
+    nombre: "buscando a nemo",
+    genero: "romance",
+  },
+  {
+    id: 14,
+    nombre: "pulp fiction",
+    genero: "romance",
+  },
+];
 
 app.get("/usuarios", (req, res) => {
   res.send(usuarios);
@@ -53,6 +125,18 @@ app.get("/unParametro/:nombre", (req, res) => {
 app.get("/ejemploQueries", (req, res) => {
   let { nombre, apellido, edad } = req.query;
   res.send(`Bienvenido, ${nombre} ${apellido} de ${edad} años`); //http://localhost:8080/ejemploQueries?nombre=joel&apellido=casta&edad=29
+});
+app.get("/peliculas", (req, res) => {
+  const { genero } = req.query;
+  const peliculasFiltradas = peliculas.filter(
+    (pelicula) => pelicula.genero === genero
+  );
+
+  if (peliculasFiltradas.length <= 0) {
+    res.send(`No existe pelicula con el genero ${genero}`);
+    return;
+  }
+  res.send(peliculasFiltradas);
 });
 
 app.listen(port, () => {
