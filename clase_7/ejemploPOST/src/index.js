6cd1fc762215859b9ec10ba52a55dd3ca95d02e2
Joel <joelcastagnani@hotmail.com>
2024-09-07 13:30:19 -0300

    70200 - 22/08 - Clase 7 - Express avanzado - 01:14 (termine el primer ej de postman)

--- clase_7/ejemploPOST/src/index.js
+++ clase_7/ejemploPOST/src/index.js
@@ -1,2 +1,30 @@
+//node --watch .\src\index.js
 import express from "express";
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const users = [];
+
+app.get("/api/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/api/users", (req, res) => {
+  const user = req.body;
+
+  if (!user.firstName || !user.lastName || !user.id) {
+    return res
+      .status(400)
+      .send({ status: "error", error: "valores incompletos" });
+  }
+
+  users.push(user);
+
+  res.send({ status: "success", message: "usuario creado" });
+});
+
+app.listen(8080, () => {
+  console.log("servidor levantado en puerto 8080, en ejemplo post");
+});
