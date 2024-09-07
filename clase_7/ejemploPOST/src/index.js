b34dbba72aeb73f1a4dabccfc83c6480eef1ad39
Joel <joelcastagnani@hotmail.com>
2024-09-07 17:19:24 -0300

    70200 - 22/08 - Clase 7 - Express avanzado - 01:37 (termina ejemplo completo de postman, no entendi NADA)

--- clase_7/ejemploPOST/src/index.js
+++ clase_7/ejemploPOST/src/index.js
@@ -5,24 +5,57 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const users = [];
+let users = [];
 
 app.get("/api/users", (req, res) => {
   res.send(users);
 });
-
 app.post("/api/users", (req, res) => {
   const user = req.body;
 
   if (!user.firstName || !user.lastName || !user.id) {
     return res
-      .status(400)
+      .status(422)
       .send({ status: "error", error: "valores incompletos" });
   }
 
   users.push(user);
 
   res.send({ status: "success", message: "usuario creado" });
+});
+app.put("/api/users/:id", (req, res) => {
+  const userID = req.params.id;
+  const updateUser = req.body;
+
+  const index = users.findIndex((user) => user.id == userID);
+
+  if (index === -1) {
+    return res
+      .status(404)
+      .send({ status: "error", messsage: "Usuario no encontrado" });
+  }
+  if (!updateUser.firstName || !updateUser.lastName || !updateUser.id) {
+    return res
+      .status(422)
+      .send({ status: "error", error: "valores incompletos" });
+  }
+
+  users[index] = updateUser;
+  res.send({ status: "success", message: "Usuario editado" });
+});
+app.delete("/api/users/:id", (req, res) => {
+  const userID = req.params.id;
+  let currentLength = users.length;
+
+  users = users.filter((user) => userID != user.id);
+
+  if (users.length == currentLength) {
+    return res
+      .status(404)
+      .send({ status: "error", error: "Usuario no encontrado" });
+  }
+
+  res.send({ status: "success", message: users });
 });
 
 app.listen(8080, () => {
