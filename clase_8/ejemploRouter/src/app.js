ee2e1db79ba901dd7f6268897b64a89844ba9425
Joel <joelcastagnani@hotmail.com>
2024-09-08 15:27:48 -0300

    27/08 - Clase 8 - Router y Multer - 00:43, ahora si, arregle el __dirname

--- clase_8/ejemploRouter/src/app.js
+++ clase_8/ejemploRouter/src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import usersRouter from "./routes/users.js";
 import petsRouter from "./routes/pets.js";
+import __dirname from "./utils.js";
 
 const app = express();
 
 app.use(express.json());
-app.use("/static", express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use("/static", express.static(__dirname + "/public"));
 
 app.use("/api/users", usersRouter);
 app.use("/api/pets", petsRouter);
